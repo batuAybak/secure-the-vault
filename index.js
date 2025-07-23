6f9a26f759f1dd7d6a69fa4cf81a34d06d6d3226
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-07-23 12:28:17 -0400

    assignment 08

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1
+
+code1 = 7 + 3 //10
+let code2 = (-20 -60) / -2 //40
+let code3 = 19 * 2 + 5 % 4 //39
+let message = 'The vault has been secured. The combination is:'
+
+
+let codeA = '10-40-39'
+let codeB = `${code1}-${code2}-${code3}`
+
+console.log(message)
+console.log(codeA)
+console.log(codeB)
